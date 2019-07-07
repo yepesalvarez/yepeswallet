b5cb7f2420da407cc06dc8f7201d89d726635727
yepesalvarez <yepesluisfernando@hotmail.com>
2019-07-07 13:53:49 -0500

    initial commit, project base setup

---
+++ Backend/YepesWallet/src/main/java/com/yepes/luis/wallet/YepesWalletApplication.java
@@ -0,0 +1,13 @@
+package com.yepes.luis.wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YepesWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YepesWalletApplication.class, args);
+	}
+
+}
